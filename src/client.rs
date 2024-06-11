58ca9f0f3cecd8bff50d9191a9720df00788eda5
Ruben Nijveld <ruben@tweedegolf.com>
2024-06-11 12:05:45 +0200

    Some changes, update docs

--- src/client.rs
+++ src/client.rs
@@ -79,7 +79,7 @@ pub struct Client {
     client_ref: Arc<ClientRef>,
 }
 
-pub struct ClientRef {
+struct ClientRef {
     client: reqwest::Client,
     base_url: url::Url,
     default_page_size: usize,
@@ -153,11 +153,14 @@ impl ClientRef {
 }
 
 impl Client {
+    /// Create a new client for a VTN located at the specified URL
     pub fn new(base_url: Url) -> Client {
         let client = reqwest::Client::new();
         Self::with_reqwest(base_url, client)
     }
 
+    /// Create a new client, but use the specific reqwest client instead of
+    /// the default one. This allows you to configure proxy settings, timeouts, etc.
     pub fn with_reqwest(base_url: Url, client: reqwest::Client) -> Client {
         Client {
             client_ref: Arc::new(ClientRef {
@@ -296,6 +299,8 @@ impl Client {
     }
 }
 
+/// A client for interacting with the data in a specific program and the events
+/// contained in the program.
 #[derive(Debug)]
 pub struct ProgramClient {
     client: Arc<ClientRef>,
@@ -310,22 +315,28 @@ impl ProgramClient {
         }
     }
 
+    /// Get the id of the program
     pub fn id(&self) -> &ProgramId {
         &self.data.id
     }
 
+    /// Get the time the program was created on the VTN
     pub fn created_date_time(&self) -> &chrono::DateTime<chrono::Utc> {
         &self.data.created_date_time
     }
 
+    /// Get the time the program was last modified on the VTN
     pub fn modification_date_time(&self) -> &chrono::DateTime<chrono::Utc> {
         &self.data.modification_date_time
     }
 
+    /// Read the data of the program
     pub fn data(&self) -> &ProgramContent {
         &self.data.content
     }
 
+    /// Modify the data of the program, make sure to update the program on the
+    /// VTN once your modifications are complete.
     pub fn data_mut(&mut self) -> &mut ProgramContent {
         &mut self.data.content
     }
@@ -347,6 +358,7 @@ impl ProgramClient {
             .await
     }
 
+    /// Create a new event on the VTN
     pub async fn create_event(&self, event_data: EventContent) -> Result<EventClient> {
         if &event_data.program_id != self.id() {
             return Err(crate::Error::InvalidParentObject);
@@ -355,6 +367,7 @@ impl ProgramClient {
         Ok(EventClient::from_event(self.client.clone(), event))
     }
 
+    /// Create a new event object within the program.
     pub fn new_event(&self) -> EventContent {
         EventContent {
             object_type: Some(EventObjectType::Event),
